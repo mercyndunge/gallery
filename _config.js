1f8d02730ff66a5a1362b35710481dabfda8ad03
mercyndunge <syokaumercy2@gmail.com>
2025-04-01 19:18:06 +0300

    Update MongoDB credentials in _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://syokaumercy2:jXZmDLCbf1w9SJgX@stagingcluster.4ydwwcf.mongodb.net/',
+    development: 'mongodb+srv://syokaumercy2:jXZmDLCbf1w9SJgX@stagingcluster.4ydwwcf.mongodb.net/',
+    test: 'mongodb+srv://syokaumercy2:jXZmDLCbf1w9SJgX@stagingcluster.4ydwwcf.mongodb.net/',
 }
 module.exports = config;
